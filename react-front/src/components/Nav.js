51162b498d7a10634e9d9282c3cf59aa109f5d05
mesepith <zahiralam29@gmail.com>
2023-01-06 15:18:51 +0530

    defin menus in navbar, give logo

--- react-front/src/components/Nav.js
+++ react-front/src/components/Nav.js
@@ -19,21 +19,21 @@ const Nav = () => {
 
     return (
         <div>
+            <img className='dash_logoz' alt="Employee Managenet" src="https://cdn.pixabay.com/photo/2022/05/10/03/05/worker-7185854_640.png"></img>
+            {!user ?
+            <ul className='header-ul menu-right'>
+                <>
+                <li><Link to="/signup">Register</Link></li> 
+                <li><Link to="/login">Login</Link></li>
+                </>
+            </ul>
+            : 
             <ul className='header-ul'>
                 <li><Link to="/">List</Link></li>
                 <li><Link to="/add">Add Employee</Link></li>
-                
-                    {!user ?
-                    <>
-                    <li><Link to="/signup">Register</Link></li> 
-                    <li><Link to="/login">Login</Link></li>
-                    </>
-                    : 
-                    <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    }
-                
-                
+                <li><Link onClick={logout} to="/signup">Logout ({user.name}) </Link></li>
             </ul>
+    }
         </div>
     )
 }
